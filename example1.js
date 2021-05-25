d5d8112bb8fc2f21f3e3e47421f41c602309a5cf
sarabastian <seb118@georgetown.edu>
2021-05-25 16:27:37 -0400

    code for medium

---
+++ example1.js
@@ -0,0 +1,10 @@
+var firstName = 'Sara';
+var age = 25;
+if (firstName) {
+    age = '24';
+}
+var foo = {
+    a: 123,
+    b: 456
+};
+foo.a;
---
+++ example1.ts
@@ -0,0 +1,15 @@
+let firstName = 'Sara'
+let age = 25
+
+
+if (firstName){
+    age = '24'
+}
+
+let foo = {
+    a: 123,
+    b: 456
+};
+
+foo.a
+
